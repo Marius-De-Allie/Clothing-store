c01f6419f6035d775870d7e26718e5c87c1fb494
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-13 13:45:49 -0400

    Declare section component state property within Directory component using useState hook, initial value of state proprty set to an empty array

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,28 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MeniItem from "./MenuItem";
 // styles
 import '../styles/directory.scss';
 
-class Directory extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-
-        }
-    }
-
-    render() {
-        return (
-            <div className="directory-menu">
-                <MeniItem title="HATS"/>
-                <MeniItem title="JACKETS" />
-                <MeniItem title="SNEAKERS" />
-                <MeniItem title="WOMENS" />
-                <MeniItem title="MENS" />
-            </div>
-        );
-    }
+const Directory = props => {
+    // component state.
+    const [section, setSection] = useState([]);
+    
+    return (
+        <div className="directory-menu">
+            <MeniItem title="HATS"/>
+            <MeniItem title="JACKETS" />
+            <MeniItem title="SNEAKERS" />
+            <MeniItem title="WOMENS" />
+            <MeniItem title="MENS" />
+        </div>
+    )
 };
 
 export default Directory;
