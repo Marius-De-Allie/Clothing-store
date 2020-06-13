bb54291a823dfab3f1576a6605e89e3c203a7e3d
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-13 14:24:40 -0400

    Add hats Route to App component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage';
 
+const Hatspage = () => (
+  <h1>HATS PAGE</h1>
+);
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={Hatspage} />
       </Switch>
     </div>
   );
