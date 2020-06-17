f142e85840a0902965e97b0f7c012f5e8abc79a1
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-16 22:29:18 -0400

    Convert CartIcon to a container component and map toggleHidden action creator as a prop on CartIcon component, add onClick handler to CartIcon comp, dispatching toggleHidden action when clicked

--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../assets/shopping-bag.svg';
+import { toggleHidden } from '../redux/actions/cart';
 // styles.
 import '../styles/cartIcon.scss';
 
-const CartIcon = () => (
-    <div className="cart-icon">
+const CartIcon = ({ toggleHidden }) => (
+    <div className="cart-icon" onClick={toggleHidden}>
         <ShoppingIcon className="shopping-icon" />
         <span className="item-count">0</span>
     </div>
 );
 
-export default CartIcon;
+const mapDispatchToprops = ({
+    toggleHidden
+});
+
+export default connect(null, mapDispatchToprops)(CartIcon);
