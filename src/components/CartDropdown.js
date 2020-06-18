3fa81ed71de78fb809a657d781beacddec573090
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-17 23:01:58 -0400

    Add New Route component to App component for Checkout route, call history.push('/checkout') on onClick handler within CartDropdown component

--- src/components/CartDropdown.js
+++ src/components/CartDropdown.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import CustomButton from './CustomButton';
 import CartItem from './CartItem';
 // styles.
 import '../styles/cartDropdown.scss';
 
-const CartDropdown = ({ cartItems }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
-        {
-            cartItems.length ?
-            cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />) :
-            <span className="empty-message">Your cart is empty</span>
-        }
+const CartDropdown = ({ cartItems }) => {
+    const history = useHistory();
+
+    return (
+        <div className="cart-dropdown">
+            <div className="cart-items">
+            {
+                cartItems.length ?
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />) :
+                <span className="empty-message">Your cart is empty</span>
+            }
+            </div>
+            <CustomButton onClick={() => history.push("/checkout")}>GO TO CHECKOUT</CustomButton>
         </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
-);
+    );
+};
 
 const mapStateToProps = ({ cart }) => ({
     cartItems: cart.items
