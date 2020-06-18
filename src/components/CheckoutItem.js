1430d3cfbbc4b3697aa90e802073695dccd52398
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-18 12:27:25 -0400

    Update CheckoutItem component's rendered JSX to include a left arrow icon and right arrow icon next to item quantity

--- src/components/CheckoutItem.js
+++ src/components/CheckoutItem.js
@@ -4,13 +4,17 @@ import { removeCartItem, addItem, removeItem } from '../redux/actions/cart';
 // styles.
 import '../styles/checkoutItem.scss';
 
-const CheckoutItem = ({ item, removeCartItem }) => (
+const CheckoutItem = ({ item, removeCartItem, addItem, removeItem }) => (
     <div className="checkout-item">
         <div className="image-container">
             <img src={item.imageUrl} alt={item.name} />
         </div>
         <span className="name">{item.name}</span>
-        <span className="quantity">{item.quantity}</span>
+        <span className="quantity">
+            <div className="arrow">&#10094;</div>
+            <span className="value">{item.quantity}</span>
+            <div className="arrow">&#10095;</div>
+        </span>
         <span className="price">{item.price}</span>
         <div className="remove-button" onClick={() => removeCartItem(item.id)}>&#10005;</div>
     </div>
