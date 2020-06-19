b40e2e94d57336e6b0d2b94a2de32be10e495651
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-18 23:31:57 -0400

    Create CategoryPage component and add new Route component to SHopPage for CategoryPage

---
+++ src/pages/CategoryPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CategoryItem from '../components/CategoryItem';
+
+import '../styles/categoryPage.scss';
+
+const CategoryPage = () => (
+    <div className="category">
+        <h1>CATEGIRY PAGE</h1>
+    </div>
+);
+
+export default CategoryPage;
--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CategoriesOverview from '../components/CategoriesOverview';
+import CategoryPage from './CategoryPage';
 
 const ShopPage = ({ match }) => (
     <div className="shop-page">
-        <Route exact path={`${match.path}/:category`} component={CategoriesOverview} />
+        <Route exact path={`${match.path}`} component={CategoriesOverview} />
+        <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
     </div>
 
 );
