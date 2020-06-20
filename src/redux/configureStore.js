95086c519e55ff5008f7992e94894f141e1b7b49
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-19 22:09:46 -0400

    Conditionally add logger middleware to middlewares array when in NODE_ENV is equal development

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,8 +3,16 @@ import{ persistStore } from 'redux-persist';
 import reducer from './reducers';
 import logger from 'redux-logger';
 
+// Array containing middleware
+const middlewares = [];
+
+// If app is in dev mode add logger middlware to middlewares array.
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+};
+
 // redux store.
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(...middlewares));
 
 const persistor = persistStore(store);
 
