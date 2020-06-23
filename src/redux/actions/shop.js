642fb265f87f8878a7e048f838e1d2e917b62d73
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-22 22:28:17 -0400

    Declare fetchCollectionsStart, fetchCollectionsSuccess, fetchCollectionsFail and fetchCollectionsAsync action creators

--- src/redux/actions/shop.js
+++ src/redux/actions/shop.js
@@ -1,8 +1,39 @@
-const UPDATE_COLLECTIONS = 'UPDATE_COLLECTIONS';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-const updateCollections = collectionsMap => ({
-    type: UPDATE_COLLECTIONS,
+const FETCH_COLLECTIONS_START = 'FETCH_COLLECTIONS_START';
+const FETCH_COLLECTIONS_SUCCESS = 'FETCH_COLLECTIONS_SUCCESS';
+const FETCH_COLLECTIONS_FAIL = 'FETCH_COLLECTIONS_FAIL';
+
+const fetchCollectionsStart = () => ({
+    type: FETCH_COLLECTIONS_START
+});
+
+const fetchCollectionsSuccess = collectionsMap => ({
+    type: FETCH_COLLECTIONS_SUCCESS,
     collectionsMap
 });
 
-export {updateCollections, UPDATE_COLLECTIONS};
+const fetchCollectionsFail = errorMessage => ({
+    type: FETCH_COLLECTIONS_FAIL,
+    errorMessage
+});
+
+// Async thunk action creator.
+const fetchCollectionsAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        // Dispatch FETCH-COLLECTIONS_START action.
+        dispatch(fetchCollectionsStart());
+        collectionRef.get().then(snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            // Dispatch fetchCollectionsSuccess action to add collections to redux store.
+            dispatch(fetchCollectionsSuccess(collectionsMap));
+        })
+        .catch(error => {
+            // Dispatch FETCH_COLLECTIONS_FAIL action.
+            dispatch(fetchCollectionsFail(error.message));
+        });
+    }
+};
+
+export { fetchCollectionsAsync, FETCH_COLLECTIONS_START, FETCH_COLLECTIONS_SUCCESS, FETCH_COLLECTIONS_FAIL };
