dc15a4518641e820a74364467575bcdf6b06b777
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-22 21:37:40 -0400

    Add redux-thunk to redux store's applyMiddlware call

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import{ persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 import reducer from './reducers';
 import logger from 'redux-logger';
 
 // Array containing middleware
-const middlewares = [];
+const middlewares = [thunk];
 
 // If app is in dev mode add logger middlware to middlewares array.
 if(process.env.NODE_ENV === 'development') {
