22d4d7ac1f6727f792228cdd70dfd046792c250a
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-22 21:08:53 -0400

    Use WitHSpinner higher order component within ShopPage component to conditionally render CategoriesOverview and CategoryPage components

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 import CategoriesOverview from '../components/CategoriesOverview';
 import CategoryPage from './CategoryPage';
+import WithSpinner from '../components/WithSpinner';
 import { firestore, convertCollectionsSnapshotToMap } from '../firebase/firebase.utils';
 import { updateCollections } from '../redux/actions/shop';
+
+const CategoriesOverviewWithSpinner = WithSpinner(CategoriesOverview);
+const CategoryPageWithSpinner = WithSpinner(CategoryPage);
 
 class ShopPage extends React.Component {
     // component state.
     state = {
         loading: true
     };
-    
+
     unsubscribeFromSnapshot = null;
     componentDidMount() {
         const { updateCollections } = this.props;
@@ -21,19 +24,23 @@ class ShopPage extends React.Component {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             // Dispatch updateCollections action.
             updateCollections(collectionsMap);
-        })
-
+            // Set loading state value to false.
+            this.setState(() => ({
+                loading: false
+            }));
+        });
     }
 
+    
     render() {
         const { match } = this.props;
+        const { loading } = this.state;
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} component={CategoriesOverview} />
-                <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+                <Route exact path={`${match.path}`} render={(props) => <CategoriesOverviewWithSpinner  isLoading={loading} {...props} />} />
+                <Route path={`${match.path}/:categoryId`} render={(props) => <CategoryPageWithSpinner isLoading={loading} {...props} />} />
             </div>
         )
-
     }
 };
 
