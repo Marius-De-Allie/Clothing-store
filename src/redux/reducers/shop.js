ea6ffb097b2c5c4175e377e528f16f5c9452b9ad
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-22 22:42:14 -0400

    Add cases for FETCH_COLLECTIONS_START, FETCH_COLLECTIONS_SUCCESS and FETCH_COLLECTIONS_FAIL action types to shop reducer

--- src/redux/reducers/shop.js
+++ src/redux/reducers/shop.js
@@ -1,15 +1,29 @@
-import { UPDATE_COLLECTIONS } from '../actions/shop';
+import { FETCH_COLLECTIONS_START, FETCH_COLLECTIONS_SUCCESS, FETCH_COLLECTIONS_FAIL } from '../actions/shop';
 
 const INITIAL_STATE = {
-    collections: null
+    collections: null,
+    isFetching: false,
+    errorMessage: undefined
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case UPDATE_COLLECTIONS:
+        case FETCH_COLLECTIONS_START:
             return {
                 ...state,
-                collections: action.collectionsMap
+                isFetching: true
+            }
+        case FETCH_COLLECTIONS_SUCCESS:
+            return {
+                ...state,
+                collections: action.collectionsMap,
+                isFetching: false
+            }
+        case FETCH_COLLECTIONS_FAIL:
+            return {
+                ...state,
+                isFetching: false,
+                errorMessage: action.errorMessage
             }
         default:
             return state;
