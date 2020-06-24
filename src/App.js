2d2fda8f1d6543633ffa2b5760ae6b2260d2fcbf
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-23 22:24:33 -0400

    Convert App component to a functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Homepage from './pages/Homepage';
@@ -11,15 +11,10 @@ import { setCurrentUser } from './redux/actions/user';
 import './App.css';
 
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-  
-  componentDidMount() {
-    const { setCurrentUser, collectionsArray } = this.props;
-    // Add new collection and documents to firebase DB (Run once).
-    // addCollectionAndDocuments('collections', collectionsArray.map(({ title, items}) => ({title, items})));
+const App = ({ setCurrentUser, collectionsArray, currentUser }) => {
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+  useEffect(() => {
+    const unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
@@ -36,26 +31,67 @@ class App extends React.Component {
         setCurrentUser(userAuth);
       } 
     })
-  }
+  }, [setCurrentUser]);
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/signin" render={() => this.props.currentUser ? <Redirect to="/"/> : <SignInPage />} />
-          <Route exact path="/checkout" component={CheckOutPage} />
-        </Switch>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/signin" render={() => currentUser ? <Redirect to="/"/> : <SignInPage />} />
+        <Route exact path="/checkout" component={CheckOutPage} />
+      </Switch>
+    </div>
+  );
 };
+  
+
+// class App extends React.Component {
+//   unsubscribeFromAuth = null;
+  
+//   componentDidMount() {
+//     const { setCurrentUser, collectionsArray } = this.props;
+//     // Add new collection and documents to firebase DB (Run once).
+//     // addCollectionAndDocuments('collections', collectionsArray.map(({ title, items}) => ({title, items})));
+
+//     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+//       if(userAuth) {
+//         const userRef = await createUserProfileDocument(userAuth);
+
+//         userRef.onSnapshot(snapShot => {
+//           console.log(snapShot.data());
+//           // Dispatch setCurrentUser action.
+//           setCurrentUser({
+//               id: snapShot.id,
+//               ...snapShot.data()
+//           });
+//         });
+
+//       } else {
+//         setCurrentUser(userAuth);
+//       } 
+//     })
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribeFromAuth();
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Header />
+//         <Switch>
+//           <Route exact path="/" component={Homepage} />
+//           <Route path="/shop" component={ShopPage} />
+//           <Route exact path="/signin" render={() => this.props.currentUser ? <Redirect to="/"/> : <SignInPage />} />
+//           <Route exact path="/checkout" component={CheckOutPage} />
+//         </Switch>
+//       </div>
+//     );
+//   }
+// };
 
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
