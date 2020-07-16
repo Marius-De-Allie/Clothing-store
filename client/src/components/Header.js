3d24e6de09f0331591ba955dcc409da44bb70238
Marius-De-Allie <spectre4300s@yahoo.com>
2020-07-16 16:52:26 -0400

    Declare onSignOut function within Header component, update signout button's onClick prop value to onSignOut callback

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,29 +5,41 @@ import { auth } from '../firebase/firebase.utils';
 import CartIcon from './CartIcon';
 import CartDropdown from './CartDropdown';
 import { ReactComponent as Logo } from '../assets/lion.svg'
+import { emptyCart } from '../redux/actions/cart';
 // styles.
 import '../styles/header.scss';
 
-const Header = ({ currentUser, hidden }) => (
-    
-    <div className="header">
-        <NavLink className="logo-container" to="/">
-            <Logo className="logo" />
-        </NavLink>
-        <div className="options">
-            <NavLink  activeClassName="active" className="option" to="/shop">SHOP</NavLink>
-            <NavLink  activeClassName="active" className="option" to="/shop">CONTACT</NavLink>
-            {
-                currentUser ? 
-                <div className="option" id="sign-out" onClick={() => auth.signOut()}>SIGN OUT</div> :
-                <NavLink  activeClassName="active" className="option" to="/signin">SIGN IN</NavLink>
-            }
-            <CartIcon />
-        </div>
-        {!hidden && <CartDropdown />}
-    </div>
-);
+const Header = ({ currentUser, hidden }) => {
+    // Sign out event handler.
+    const onSignOut = () => {
+        // sign out user via firebase.
+        auth.signOut();
+        // Empty shopping cart.
 
+
+
+    }
+    
+    return (
+        <div className="header">
+            <NavLink className="logo-container" to="/">
+                <Logo className="logo" />
+            </NavLink>
+            <div className="options">
+                <NavLink  activeClassName="active" className="option" to="/shop">SHOP</NavLink>
+                <NavLink  activeClassName="active" className="option" to="/shop">CONTACT</NavLink>
+                {
+                    currentUser ? 
+                    <div className="option" id="sign-out" onClick={onSignOut}>SIGN OUT</div> :
+                    <NavLink  activeClassName="active" className="option" to="/signin">SIGN IN</NavLink>
+                }
+                <CartIcon />
+            </div>
+            {!hidden && <CartDropdown />}
+        </div>
+    );
+};
+    
 const mapStateToProps = ({ user, cart }) => ({
     currentUser: user.currentUser,
     hidden: cart.hidden
