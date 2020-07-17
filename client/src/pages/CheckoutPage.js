d782f0bd43a7badc4a21e78846c49663dd310bc6
Marius-De-Allie <spectre4300s@yahoo.com>
2020-07-16 21:22:38 -0400

    declare onEmptyCart function within CheckoutPage component and set empty cart element's onClick prop value to onEmptyCart callback

--- client/src/pages/CheckoutPage.js
+++ client/src/pages/CheckoutPage.js
@@ -8,38 +8,47 @@ import { emptyCart } from '../redux/actions/cart';
 // styles
 import '../styles/checkOutPage.scss'
 
-const CheckOutPage = ({ items, totalPrice }) => (
-    <div className="checkout-page">
-        <div className="checkout-header">
-            <div className="header-block">
-                <span>Product</span>
+const CheckOutPage = ({ items, totalPrice, emptyCart }) => {
+
+    // Handle empty cart click event.
+    const onEmptyCart = () => {
+        // dispatch EMPTY_CART action.
+        emptyCart();
+    };
+
+    return (
+        <div className="checkout-page">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
             </div>
-            <div className="header-block">
-                <span>Description</span>
+            {items.map(item => <CheckoutItem key={item.id} item={item} />)}
+            <div className="empty-cart" onClick={onEmptyCart}>empty cart<FontAwesomeIcon className="trash-icon" icon={faTrash} /></div>
+            <div className="total">
+                <span>TOTAL: ${totalPrice}</span>
             </div>
-            <div className="header-block">
-                <span>Quantity</span>
+            <div className="test-warning">
+                *Please use the following test credit card for payments*
+                <br />
+                4242-4242-4242-4242 - Exp: 01/25 - CVV: 123
             </div>
-            <div className="header-block">
-                <span>Price</span>
-            </div>
-            <div className="header-block">
-                <span>Remove</span>
-            </div>
+            <StripeCheckoutButton price={totalPrice}/>
         </div>
-        {items.map(item => <CheckoutItem key={item.id} item={item} />)}
-        <div className="empty-cart" onClick={() => console.log('empty cart clicked')}>empty cart<FontAwesomeIcon className="trash-icon" icon={faTrash} /></div>
-        <div className="total">
-            <span>TOTAL: ${totalPrice}</span>
-        </div>
-        <div className="test-warning">
-            *Please use the following test credit card for payments*
-            <br />
-            4242-4242-4242-4242 - Exp: 01/25 - CVV: 123
-        </div>
-        <StripeCheckoutButton price={totalPrice}/>
-    </div>
-);
+    );
+};
 
 const mapStateToProps = ({ cart }) => ({
     items: cart.items,
@@ -47,7 +56,7 @@ const mapStateToProps = ({ cart }) => ({
 });
 
 const mapDispatchToProps = {
-    emptyCart;
+    emptyCart
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CheckOutPage);
