382ae0d43e7add62c4599ae4933cea4912ef5d41
Marius-De-Allie <spectre4300s@yahoo.com>
2020-07-25 22:31:51 -0400

    Create notfoundPage.scss stylesheet for NotfoundPage component, import stylesheet into notFoundPage component

--- client/src/pages/NotfoundPage.js
+++ client/src/pages/NotfoundPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// Styles.
+import '../styles/notfoundPage.scss';
 
 const NotfoundPage = () => (
-    <div>
-        404 page not found.
-        <Link to="/">Return Home</Link>
+    <div className="not-found-container">
+        <h2>404 page not found</h2>
+        <Link className="home-link" to="/">Return Home</Link>
     </div>
 );
 
